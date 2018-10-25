0b00021d5ed4812f9657f17bd5ebb08df1f4b3b5
egonm12 <egonmeijers12@gmail.com>
2018-10-25 21:14:56 +0200

    Added test for counting nodes on postconnection

--- backend/app/graphql/kudos_backend_schema.rb
+++ backend/app/graphql/kudos_backend_schema.rb
@@ -5,4 +5,7 @@ KudosBackendSchema = GraphQL::Schema.define do
 
   mutation(Types::MutationType)
   query(Types::QueryType)
+
+  # solution for: https://github.com/rmosolgo/graphql-ruby/issues/1922
+  GraphQL::Relay::BaseConnection.register_connection_implementation(ActiveRecord::Relation, GraphQL::Relay::RelationConnection)
 end
