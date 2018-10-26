7ddf2b18eb5839c93f9aa7b8334ca8517f196b03
egonm12 <egonmeijers12@gmail.com>
2018-10-26 09:10:41 +0200

    Resolved feedback and lint fix

--- backend/app/graphql/kudos_backend_schema.rb
+++ backend/app/graphql/kudos_backend_schema.rb
@@ -5,7 +5,4 @@ KudosBackendSchema = GraphQL::Schema.define do
 
   mutation(Types::MutationType)
   query(Types::QueryType)
-
-  # solution for: https://github.com/rmosolgo/graphql-ruby/issues/1922
-  GraphQL::Relay::BaseConnection.register_connection_implementation(ActiveRecord::Relation, GraphQL::Relay::RelationConnection)
 end
